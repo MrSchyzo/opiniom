1bc6783caf62dd2214f49bf41def4c55afa4f5e3
MrSchyzo <catapano-marco@mail.com>
2022-08-07 16:34:44 +0200

    Adds if, identity, unless

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ val projectPath = "$whoami/$projectName"
 
 allprojects {
     group = "io.github.mrschyzo"
-    version = "0.1.0"
+    version = "0.2.0"
 }
 
 java {
--- src/test/kotlin/io/github/mrschyzo/opiniom/functional/FunctionExtsKtTest.kt
+++ src/test/kotlin/io/github/mrschyzo/opiniom/functional/FunctionExtsKtTest.kt
@@ -3,6 +3,7 @@ package io.github.mrschyzo.opiniom.functional
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
+import strikt.assertions.isSameInstanceAs
 
 internal class FunctionExtsKtTest {
 
@@ -15,6 +16,12 @@ internal class FunctionExtsKtTest {
 
         expectThat(composition(5))
             .isEqualTo("1,2,3,4,5")
+    }
+
+    @Test
+    fun `identity returns the SAME reference object`() {
+        expectThat(identity(5))
+            .isSameInstanceAs(5)
     }
 
     @Test
@@ -31,6 +38,70 @@ internal class FunctionExtsKtTest {
 
         expectThat(flipped("_postfix", "prefix"))
             .isEqualTo("prefix_postfix")
+    }
+
+    @Test
+    fun `function if false = identity`() {
+        val foo = { x: Int -> x + 1 } `if` false
+
+        expectThat(foo(10))
+            .isEqualTo(10)
+    }
+
+    @Test
+    fun `function if true = function`() {
+        val foo = { x: Int -> x + 1 } `if` true
+
+        expectThat(foo(50))
+            .isEqualTo(51)
+    }
+
+    @Test
+    fun `function if lazy-false = identity`() {
+        val foo = { x: Int -> -x } `if` { x: Int -> x < 0 }
+
+        expectThat(foo(10))
+            .isEqualTo(10)
+    }
+
+    @Test
+    fun `function if lazy-true = function`() {
+        val foo = { x: Int -> -x } `if` { x: Int -> x < 0 }
+
+        expectThat(foo(-1))
+            .isEqualTo(1)
+    }
+
+    @Test
+    fun `function unless false = function`() {
+        val foo = { x: Int -> x + 1 } unless false
+
+        expectThat(foo(10))
+            .isEqualTo(11)
+    }
+
+    @Test
+    fun `function unless true = identity`() {
+        val foo = { x: Int -> x + 1 } unless true
+
+        expectThat(foo(50))
+            .isEqualTo(50)
+    }
+
+    @Test
+    fun `function unless lazy-false = function`() {
+        val foo = { x: Int -> -x } unless { x: Int -> x < 0 }
+
+        expectThat(foo(10))
+            .isEqualTo(-10)
+    }
+
+    @Test
+    fun `function unless lazy-true = identity`() {
+        val foo = { x: Int -> -x } unless { x: Int -> x < 0 }
+
+        expectThat(foo(-1))
+            .isEqualTo(-1)
     }
 
     @Test
